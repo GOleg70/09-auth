b59dfe8d8494072f9fddbb09c5f0b82fca00f0b7
Oleg Ganus <oleg110770@gmail.com>
2025-09-15 17:39:59 +0300

    edit 1

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,22 +1,67 @@
-// components/Header/Header.tsx
-import Link from "next/link";
-import css from "./Header.module.css";
-import TagsMenu from "../TagsMenu/TagsMenu";
+// import Link from "next/link";
+// import css from "./Header.module.css";
+// import TagsMenu from "../TagsMenu/TagsMenu";
 
-export default function Header() {
+// export default function Header() {
+//   return (
+//     <header className={css.header}>
+//       <Link href="/" aria-label="Home" className={css.logo}>
+//         NoteHub
+//       </Link>
+//       <nav aria-label="Main Navigation">
+//         <ul className={css.navigation}>
+//           <li>
+//             <Link href="/">Home</Link>
+//           </li>
+//           <li><TagsMenu /></li>
+//         </ul>
+//       </nav>
+//     </header>
+//   );
+// }
+'use client';
+
+import { useAuthStore } from '../../lib/store/authStore';
+
+import AuthNavigation from '../AuthNavigation/AuthNavigation';
+import TagsMenu from '../TagsMenu/TagsMenu';
+import css from './Header.module.css';
+
+import Link from 'next/link';
+
+const Header = () => {
+  const { isAuthenticated } = useAuthStore();
   return (
     <header className={css.header}>
-      <Link href="/" aria-label="Home" className={css.logo}>
+      <Link className={css.headerLink} href="/" aria-label="Home">
         NoteHub
       </Link>
       <nav aria-label="Main Navigation">
         <ul className={css.navigation}>
-          <li>
-            <Link href="/">Home</Link>
+          <li className={css.navigationItem}>
+            <Link className={css.navigationLink} href="/">
+              Home
+            </Link>
           </li>
-          <li><TagsMenu /></li>
+          <li className={css.navigationItem}>
+            <Link
+              href="/profile"
+              prefetch={false}
+              className={css.navigationLink}
+            >
+              Profile
+            </Link>
+          </li>
+          {isAuthenticated && (
+            <li className={css.navigationItem}>
+              <TagsMenu />
+            </li>
+          )}
+          <AuthNavigation />
         </ul>
       </nav>
     </header>
   );
-}
+};
+
+export default Header;
---
+++ lib/api/serverApi.ts
@@ -0,0 +1,91 @@
+import { cookies } from 'next/headers';
+import { Note, CreateNote } from '../../types/note';
+import { User } from '../../types/user';
+import { nextServer } from './api';
+
+interface NotesHttpResponse {
+  notes: Note[];
+  totalPages: number;
+}
+
+export const getServerNotes = async (
+  query: string,
+  page: number,
+  tag?: string
+): Promise<NotesHttpResponse> => {
+  const PARAMS = new URLSearchParams({
+    ...(query !== '' ? { search: query } : {}),
+    ...(tag !== undefined ? { tag } : {}),
+    page: page.toString(),
+  });
+  const cookieStore = await cookies();
+
+  const response = await nextServer.get('/notes', {
+    params: PARAMS,
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
+
+  return response.data;
+};
+
+export const createServerNote = async (newNote: CreateNote): Promise<Note> => {
+  const cookieStore = await cookies();
+  const response = await nextServer.post('/notes', newNote, {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
+  return response.data;
+};
+
+export const fetchServerNoteById = async (id: string): Promise<Note> => {
+  const cookieStore = await cookies();
+  const response = await nextServer.get(`/notes/${id}`, {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
+  return response.data;
+};
+
+export const deleteServerNote = async (id: string) => {
+  const cookieStore = await cookies();
+  const response = await nextServer.delete<Note>(`/notes/${id}`, {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
+  return response.data;
+};
+
+export const editProfile = async (data: User) => {
+  const cookieStore = await cookies();
+  const res = await nextServer.patch('/users/me', data, {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
+  return res.data;
+};
+
+export const getServerMe = async (): Promise<User> => {
+  const cookieStore = await cookies();
+  const { data } = await nextServer.get('/users/me', {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
+  return data;
+};
+
+export const checkSession = async () => {
+  const cookieStore = await cookies();
+  const response = await nextServer.get('/auth/session', {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
+  return response;
+};
